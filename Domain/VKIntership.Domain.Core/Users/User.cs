ff1361f195af5d436003dde0be55b78f36abb12f
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-05-06 22:25:01 +0300

    feat: add user

---
+++ Domain/VKIntership.Domain.Core/Users/User.cs
@@ -0,0 +1,33 @@
+﻿using VKIntership.Domain.Core.Tools;
+
+#pragma warning disable CS8618
+
+namespace VKIntership.Domain.Core.Users;
+
+public class User
+{
+    protected User() { }
+
+    public User(
+        Guid id,
+        string login,
+        string password,
+        DateTime createDate,
+        string userGroup,
+        string userState)
+    {
+        Id = id;
+        Login = login;
+        Password = PasswordHasher.Hash(password);
+        CreateDate = createDate;
+        UserGroup = userGroup;
+        UserState = userState;
+    }
+
+    public Guid Id { get; }
+    public string Login { get; }
+    public string Password { get; }
+    public DateTime CreateDate { get; }
+    public string UserGroup { get; }
+    public string UserState { get; }
+}
