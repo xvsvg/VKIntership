b9c9e395c17dbf2bf2f46a2d8f0608d107376ffc
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-05-06 23:24:31 +0300

    feat: add application abstractions

---
+++ Application/VKIntership.Application.Abstractions/ApplicationRoles.cs
@@ -0,0 +1,7 @@
+﻿namespace VKIntership.Application.Abstractions;
+
+public static class ApplicationRoles
+{
+    public const string Admin = "Admin";
+    public const string User = "User";
+}
---
+++ Application/VKIntership.Application.Abstractions/ApplicationUserState.cs
@@ -0,0 +1,7 @@
+﻿namespace VKIntership.Application.Abstractions;
+
+public static class ApplicationUserState
+{
+    public const string Active = "Active";
+    public const string Blocked = "Blocked";
+}
