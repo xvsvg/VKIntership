37bdee868835b5711f5225e38ddd84736a3d8ad8
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-05-06 22:24:35 +0300

    feat: add domain abstractions

---
+++ Domain/VKIntership.Domain.Core/Abstractions/UserRole.cs
@@ -0,0 +1,7 @@
+﻿namespace VKIntership.Domain.Core.Abstractions;
+
+public static class UserRole
+{
+    public const string Admin = "Admin";
+    public const string User = "User";
+}
---
+++ Domain/VKIntership.Domain.Core/Abstractions/UserState.cs
@@ -0,0 +1,7 @@
+﻿namespace VKIntership.Domain.Core.Abstractions;
+
+public static class UserState
+{
+    public const string Active = "Active";
+    public const string Blocked = "Blocked";
+}
