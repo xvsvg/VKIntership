c72e1cc37eeadf3e9d45e49b8682e44aa581e174
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-05-06 23:21:48 +0300

    feat: add user mapping

---
+++ Infrastructure/VKIntership.Infrastructure.Mapping/Users/UserMapping.cs
@@ -0,0 +1,17 @@
+﻿using VKIntership.Application.Dto;
+using VKIntership.Domain.Core.Users;
+
+namespace VKIntership.Infrastructure.Mapping.Users;
+
+public static class UserMapping
+{
+    public static UserDto ToDto(this User user)
+    {
+        return new UserDto(
+            user.Id,
+            user.Login,
+            user.CreateDate,
+            user.UserGroup.Id,
+            user.UserState.Id);
+    }
+}
