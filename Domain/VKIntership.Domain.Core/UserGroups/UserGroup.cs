74dfbc5084dfe3cd60ed3d2fc43a2752dc30f713
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-05-06 22:24:55 +0300

    feat: add user group

---
+++ Domain/VKIntership.Domain.Core/UserGroups/UserGroup.cs
@@ -0,0 +1,21 @@
+﻿#pragma warning disable CS8618
+
+namespace VKIntership.Domain.Core.UserGroups;
+
+public class UserGroup
+{
+    protected UserGroup()
+    {
+    }
+
+    public UserGroup(Guid id, string role, string? description)
+    {
+        Id = id;
+        Role = role;
+        Description = description ?? string.Empty;
+    }
+
+    public Guid Id { get; }
+    public string Role { get; }
+    public string Description { get; }
+}
