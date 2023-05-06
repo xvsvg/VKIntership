f3c345d6a8092849e660efed924ab2eedd6dbec6
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-05-06 23:21:22 +0300

    feat: add database context contract

---
+++ Application/VKIntership.Application.DataAccess.Abstractions/IDatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using VKIntership.Domain.Core.Users;
+using VKIntership.Domain.Core.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace VKIntership.Application.DataAccess.Abstractions;
+
+public interface IDatabaseContext
+{
+    DbSet<User> Users { get; }
+
+    DbSet<UserState> UserStates { get; }
+
+    DbSet<UserRole> UserGroups { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
