3dbd37ef28f9084ac86a93daf25eaebe174a4253
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-05-06 23:07:10 +0300

    feat: add postgres configuration

---
+++ Infrastructure/VKIntership.Infrastructure.DataAccess/Configuration/PostgresConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace VKIntership.Infrastructure.DataAccess.Configuration;
+
+public class PostgresConfiguration
+{
+    public string Host { get; init; } = string.Empty;
+    public int Port { get; init; }
+    public string Username { get; init; } = string.Empty;
+    public string Password { get; init; } = string.Empty;
+
+    public string ToConnectionString(string databaseName)
+    {
+        return $"Host={Host};Port={Port};Database={databaseName};Username={Username};Password={Password};";
+    }
+}
