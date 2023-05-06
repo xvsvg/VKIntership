572f5a65341163e3caaa6522d62afbce46d8f4b4
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-05-06 23:21:03 +0300

    feat: add database context

---
+++ Infrastructure/VKIntership.Infrastructure.DataAccess/Context/DatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using VKIntership.Application.DataAccess.Abstractions;
+using VKIntership.Domain.Core.Abstractions;
+using VKIntership.Domain.Core.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace VKIntership.Infrastructure.DataAccess.Context;
+
+public class DatabaseContext : DbContext, IDatabaseContext
+{
+    public DatabaseContext(DbContextOptions<DatabaseContext> options)
+        : base(options) { }
+
+    public DbSet<User> Users { get; } = null!;
+    public DbSet<UserState> UserStates { get; } = null!;
+    public DbSet<UserRole> UserGroups { get; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(IAssemblyMarker).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
