32092003ccede4a1107b934eb458b4b73adef0e4
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-07 05:20:11 +0300

    feat: add handlers DI

---
+++ Application/VKIntership.Application.Handlers/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VKIntership.Application.Contracts.Tools;
+
+namespace VKIntership.Application.Handlers.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddHandlers(this IServiceCollection collection, IConfiguration configuration)
+    {
+        IConfigurationSection paginationSection = configuration.GetSection("Pagination");
+        collection.Configure<PaginationConfiguration>(x => paginationSection.Bind(x));
+
+        collection.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining(typeof(IAssemblyMarker)));
+
+        return collection;
+    }
+}
