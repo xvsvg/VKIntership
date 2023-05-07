511580741526f0035075ad5c0bf4089b0ca20b80
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-07 18:35:23 +0300

    feat: add seeder

---
+++ Presentation/VKIntership.Presentation.WebAPI/Helpers/SeedingHelper.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using VKIntership.Application.Contracts.UserRoles.Commands;
+using VKIntership.Application.Contracts.UserStatus.Commands;
+
+namespace VKIntership.Presentation.WebAPI.Helpers;
+
+internal static class SeedingHelper
+{
+    internal static async Task SeedUserGroups(IServiceProvider provider)
+    {
+        IMediator mediator = provider.GetRequiredService<IMediator>();
+        ILogger<Program> logger = provider.GetRequiredService<ILogger<Program>>();
+
+        try
+        {
+            await mediator.Send(new CreateUserRoles.Command());
+        }
+        catch (Exception ex)
+        {
+            logger.LogWarning(ex.Message);
+        }
+    }
+
+    internal static async Task SeedUserStates(IServiceProvider provider)
+    {
+        IMediator mediator = provider.GetRequiredService<IMediator>();
+        ILogger<Program> logger = provider.GetRequiredService<ILogger<Program>>();
+
+        try
+        {
+            await mediator.Send(new CreateUserStatuses.Command());
+        }
+        catch (Exception ex)
+        {
+            logger.LogWarning(ex.Message);
+        }
+    }
+
+}
