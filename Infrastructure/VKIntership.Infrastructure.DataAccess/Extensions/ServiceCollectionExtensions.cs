d1f0cda1ef66889b3b1feb549796f721378109f6
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-07 05:01:53 +0300

    feat: add data access DI

---
+++ Infrastructure/VKIntership.Infrastructure.DataAccess/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using VKIntership.Application.DataAccess.Abstractions;
+using VKIntership.Infrastructure.DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace VKIntership.Infrastructure.DataAccess.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddDatabase(
+        this IServiceCollection collection,
+        Action<DbContextOptionsBuilder> action)
+    {
+        collection.AddDbContext<IDatabaseContext, DatabaseContext>(action);
+    }
+}
