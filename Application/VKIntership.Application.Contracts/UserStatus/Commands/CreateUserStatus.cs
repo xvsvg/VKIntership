0c398fc0b21c84d1d00fb17002efa1b9652f7362
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-07 03:23:42 +0300

    feat: update create user status contract

--- Application/VKIntership.Application.Contracts/UserStatus/Commands/CreateUserStatus.cs
+++
@@ -1,8 +0,0 @@
-﻿using MediatR;
-
-namespace VKIntership.Application.Contracts.UserStatus.Commands;
-
-internal static class CreateUserStatus
-{
-    public record Command(string Status, string? Description) : IRequest;
-}
---
+++ Application/VKIntership.Application.Contracts/UserStatus/Commands/CreateUserStatuses.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace VKIntership.Application.Contracts.UserStatus.Commands;
+
+internal static class CreateUserStatuses
+{
+    public record Command() : IRequest;
+}
