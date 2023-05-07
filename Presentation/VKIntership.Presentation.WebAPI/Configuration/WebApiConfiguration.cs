372ef27b30037316fc42c2ef29370aba08d77d26
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-07 18:35:06 +0300

    feat: add web api config

---
+++ Presentation/VKIntership.Presentation.WebAPI/Configuration/WebApiConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using VKIntership.Infrastructure.DataAccess.Configuration;
+
+namespace VKIntership.Presentation.WebAPI.Configuration;
+
+internal class WebApiConfiguration
+{
+    public WebApiConfiguration(IConfiguration configuration)
+    {
+        if (configuration == null)
+            throw new ArgumentNullException(nameof(configuration));
+
+        PostgresConfiguration? postgresConfiguration = configuration
+            .GetSection(nameof(PostgresConfiguration))
+            .Get<PostgresConfiguration>();
+
+        PostgresConfiguration = postgresConfiguration ??
+                                throw new ArgumentException(nameof(PostgresConfiguration));
+    }
+
+    public PostgresConfiguration PostgresConfiguration { get; }
+}
