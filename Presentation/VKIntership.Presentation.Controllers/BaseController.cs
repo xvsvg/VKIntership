644f1bce1cfacc96c69c3f6ab1b56118ab89c45a
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-07 18:33:14 +0300

    feat: add controllers

---
+++ Presentation/VKIntership.Presentation.Controllers/BaseController.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+#pragma warning disable CS8618
+
+namespace VKIntership.Presentation.Controllers;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public abstract class BaseController : ControllerBase
+{
+    private protected IMediator _mediator;
+
+    protected IMediator Mediator
+    {
+        get
+        {
+            IMediator? service = HttpContext.RequestServices.GetService<IMediator>();
+
+            if (_mediator is null)
+            {
+                if (service is null)
+                    throw new Exception();
+
+                _mediator = service;
+            }
+
+            return _mediator;
+        }
+    }
+}
---
+++ Presentation/VKIntership.Presentation.Controllers/UserController.cs
@@ -0,0 +1,78 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using VKIntership.Application.Contracts.Users.Commands;
+using VKIntership.Application.Contracts.Users.Queries;
+using VKIntership.Application.Dto;
+using VKIntership.Domain.Common;
+
+namespace VKIntership.Presentation.Controllers;
+
+public class UserController : BaseController
+{
+    public UserController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet("{userId:guid}")]
+    [ProducesResponseType(200)]
+    public async Task<ActionResult<UserDto?>> GetUser(Guid userId)
+    {
+        try
+        {
+            var query = new GetUser.Query(userId);
+            var response = await _mediator.Send(query);
+            return Ok(response);
+        }
+        catch (EntityNotFoundException ex)
+        {
+            return NotFound(ex.Message);
+        }
+    }
+
+    [HttpGet]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(204)]
+    public async Task<ActionResult<IEnumerable<UserDto?>>> GetAllUsers([FromQuery] int? page, CancellationToken cancellationToken)
+    {
+        var query = new GetAllUsers.Query(page ?? 0);
+        var response = await _mediator.Send(query, cancellationToken);
+        return Ok(response);
+    }
+
+    [HttpPost("/create")]
+    [ProducesResponseType(201)]
+    public async Task<ActionResult<UserDto>> CreateUser(
+        [FromBody] string login,
+        string password,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            var command = new CreateUser.Command(login, password);
+            var response = await _mediator.Send(command, cancellationToken);
+            await Task.Delay(5000, cancellationToken);
+            return Ok(response);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpDelete("{userId:guid}")]
+    public async Task<ActionResult<UserDto>> DeleteUser(Guid userId)
+    {
+        try
+        {
+            var command = new DeleteUser.Command(userId);
+            var response = await _mediator.Send(command);
+            return Ok(response);
+        }
+        catch (EntityNotFoundException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+}
