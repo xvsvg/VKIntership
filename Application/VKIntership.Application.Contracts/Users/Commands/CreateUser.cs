b185f8cee04e89b5e392473da784c55cfd41f7a6
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-07 05:02:53 +0300

    refactor: make create user contract internal

--- Application/VKIntership.Application.Contracts/Users/Commands/CreateUser.cs
+++ Application/VKIntership.Application.Contracts/Users/Commands/CreateUser.cs
@@ -3,7 +3,7 @@ using MediatR;
 
 namespace VKIntership.Application.Contracts.Users.Commands;
 
-public static class CreateUser
+internal static class CreateUser
 {
     public record Command(string Login, string Password) : IRequest<Response>;
 
