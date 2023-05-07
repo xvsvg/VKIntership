eb7c53e292f7d0ae976c48296ae31739d737d9aa
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-07 18:35:40 +0300

    feat: modify entrypoint

--- Presentation/VKIntership.Presentation.WebAPI/Program.cs
+++ Presentation/VKIntership.Presentation.WebAPI/Program.cs
@@ -1,6 +1,42 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.EntityFrameworkCore;
+using VKIntership.Application.Handlers.Extensions;
+using VKIntership.Infrastructure.DataAccess.Extensions;
+using VKIntership.Presentation.WebAPI.Configuration;
+using VKIntership.Presentation.WebAPI.Extensions;
+using VKIntership.Presentation.WebAPI.Helpers;
 
-app.MapGet("/", () => "Hello World!");
+namespace VKIntership.Presentation.WebAPI;
 
-app.Run();
+internal class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Host.UseSerilogForAppLogs(builder.Configuration);
+
+        var webApiConfiguration = new WebApiConfiguration(builder.Configuration);
+
+        builder.Services.AddDatabase(o =>
+            o.UseNpgsql(webApiConfiguration.PostgresConfiguration.ToConnectionString("db")));
+
+        builder.Services.AddHandlers(builder.Configuration);
+
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        using (IServiceScope scope = app.Services.CreateScope())
+        {
+            await SeedingHelper.SeedUserGroups(scope.ServiceProvider);
+            await SeedingHelper.SeedUserStates(scope.ServiceProvider);
+        }
+
+        await app.RunAsync();
+    }
+}
